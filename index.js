fb1119a98bd57d741d10485f35a3da658a7848c4
x6Nenko <107570914+x6Nenko@users.noreply.github.com>
2023-06-12 21:36:41 +0300

    Add files via upload
    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener('mousemove', e => {
+    console.log(e.clientX - window.innerWidth / 2);
+  Object.assign(document.documentElement, {
+    style: `
+    --move-x: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
+    --move-y: ${(e.clientY - window.innerHeight / 2) * -0.01}deg;
+    `
+  })  
+})
